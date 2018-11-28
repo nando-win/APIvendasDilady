d33c73e95ff71914b5a04b91f069b2f5f51c9a31
Fernando Winston Cavalcante Bezerra <fernando.wcb@gmail.com>
2018-11-28 03:20:59 -0300

    Corrigido função delete

---
+++ VendasDiladyAPI/Controllers/VendasController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VendasDiladyAPI.Models;
+
+namespace VendasDiladyAPI.Controllers
+{
+    public class VendasController : ApiController
+    {
+        private static List<Vendas> vendas = new List<Vendas>();
+
+        public List<Vendas> Get()
+        {
+            return vendas;
+        }
+
+        public void Post(string nome, string data, int formaPagamento, int codProduto, string descricao, int quantidade)
+        {
+            if (!string.IsNullOrEmpty(nome))
+                vendas.Add(new Vendas(nome, data, formaPagamento, codProduto, descricao, quantidade));
+        }
+
+        public void Delete(string nome, string data, int formaPagamento, int codProduto, string descricao, int quantidade)
+        {
+            vendas.RemoveAt(vendas.IndexOf(vendas.First(x => x.NomeCliente.Equals(nome))));
+        }
+    }
+}
---
+++ VendasDiladyAPI/Models/Vendas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VendasDiladyAPI.Models
+{
+    public class Vendas
+    {
+        public string NomeCliente { get; set; }
+        public string DataPedido { get; set; }
+        public int FormaPagamento { get; set; }
+        public int CodProduto { get; set; }
+        public string DescricaoProduto { get; set; }
+        public int Quantidade { get; set; }
+
+        public Vendas(string nome, string data, int formaPagamento, int codProduto, string descricao, int quantidade)
+        {
+            this.NomeCliente = nome;
+            this.DataPedido = data;
+            this.FormaPagamento = formaPagamento;
+            this.CodProduto = codProduto;
+            this.DescricaoProduto = descricao;
+            this.Quantidade = quantidade;
+        }
+    }
+}
